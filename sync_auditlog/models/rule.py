12aafc5056cfd6b6b87718624841a7754be7c0ca
Daniel Reis <dreis@opensourceintegrators.com>
2021-04-30 16:50:46 +0100

    [WIP] sync_auditlog: fix method capture logic

--- sync_auditlog/models/rule.py
+++ sync_auditlog/models/rule.py
@@ -1,7 +1,6 @@
 # Copyright (C) 2021 Open Source Integrators
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
-import copy
 import logging
 import uuid
 from datetime import datetime
@@ -19,7 +18,7 @@ class AuditlogRule(models.Model):
     log_type = fields.Selection(selection_add=[("no_log", "No Log (Sync)")])
 
     sync_type = fields.Selection(
-        [("transaction", "Transaction"), ("master", "Master Data")],
+        [("transaction", "Transaction"), ("master", "Master Data")]
     )
     domain_filter = fields.Char()
     black_list_fields = fields.Many2many(
@@ -55,283 +54,97 @@ class AuditlogRule(models.Model):
                 methods.append("write")
             if rule.log_unlink:
                 methods.append("unlink")
-            for method in methods:
-                check_attr = "sync_auditlog_ruled_" + method
-                if not hasattr(model, check_attr) and hasattr(model, method):
-                    model._patch_method(method, rule._make_sync(method))
+            for method_name in methods:
+                check_attr = "sync_auditlog_ruled_" + method_name
+                if not hasattr(model, check_attr) and hasattr(model, method_name):
+                    patched_method = (
+                        rule._make_sync_create()
+                        if method_name == "create"
+                        else rule._make_sync_call(method_name)
+                    )
+                    model._patch_method(method_name, patched_method)
                     setattr(type(model), check_attr, True)
-                    _logger.debug("Watching %s.%s", model, method)
+                    _logger.debug("Watching %s.%s", model, method_name)
         return res
 
-    def _make_sync(self, method):
-        """Log a sync record for a watched method."""
+    def _make_sync_create(self):
+        """
+        Log a sync record for an @api.model create call.
+        A top level create call will store a regular sync log record.
+        A subsequent create call will store a simplified sync log record,
+        only with the record ID.
+        """
         self.ensure_one()
-        log_type = self.log_type
+        sync_type = self.sync_type
 
-        def synced_call(self, *args, **kwargs):
-            result = synced_call.origin(self, *args, **kwargs)
-            self = self.with_context(auditlog_disabled=True)
-            skip_sync = False
-            context = dict(self._context)
-            if self.env.context.get("auditlog_is_capturing"):
-                skip_sync = True
-            else:
-                self.env.context = dict(self.env.context)
-                self.env.context.update({"auditlog_is_capturing": True})
-            rule_model = self.env["auditlog.rule"]
-            if log_type != "no_log":
-                additional_log_values = {
-                    "log_type": log_type,
-                    "uuid": uuid.uuid4(),
-                    "timestamp": datetime.now(),
-                }
-                rule_model.sudo().create_extra_logs(
-                    self.env.uid, self._name, self.ids, method, additional_log_values,
-                )
-            if not skip_sync:
+        @api.model_create_multi
+        @api.returns("self", lambda value: value.id)
+        def logged_create_call(self, vals_list, **kwargs):
+            if sync_type:
+                doing_sync = self.env.context.get("sync_auditlog_working")
+                if not doing_sync:
+                    self = self.with_context(sync_auditlog_working=True)
                 additional_log_values = {
                     "log_type": "no_log",
                     "uuid": uuid.uuid4(),
                     "timestamp": datetime.now(),
                     "resource_ids": self.ids,
-                    "raw_args_kwargs": args,
-                    "context": context,
                     "state": "captured",
                 }
-                rule_model.sudo().create_extra_logs(
-                    self.env.uid, self._name, self.ids, method, additional_log_values,
-                )
-            return result
-
-        return synced_call
-
-    def create_extra_logs(
-        self, uid, res_model, res_ids, method, additional_log_values=None,
-    ):
-        """Create logs. `old_values` and `new_values` are dictionaries, e.g:
-        {RES_ID: {'FIELD': VALUE, ...}}
-        """
-        log_model = self.env["auditlog.log"]
-        http_request_model = self.env["auditlog.http.request"]
-        http_session_model = self.env["auditlog.http.session"]
-        for res_id in res_ids:
-            model_model = self.env[res_model]
-            name = model_model.browse(res_id).name_get()
-            res_name = name and name[0] and name[0][1]
-            vals = {
-                "name": res_name,
-                "model_id": self.pool._auditlog_model_cache[res_model],
-                "res_id": res_id,
-                "method": method,
-                "user_id": uid,
-                "http_request_id": http_request_model.current_http_request(),
-                "http_session_id": http_session_model.current_http_session(),
-            }
-            vals.update(additional_log_values or {})
-            log_model.create(vals)
-
-    def _make_create(self):
-        """Instanciate a create method that log its calls."""
-        self.ensure_one()
-        log_type = self.log_type
-        black_list_fields = [field.name for field in self.black_list_fields]
-        white_list_fields = [field.name for field in self.white_list_fields]
-
-        @api.model_create_multi
-        @api.returns("self", lambda value: value.id)
-        def create_full(self, vals_list, **kwargs):
-            self = self.with_context(auditlog_disabled=True)
-            rule_model = self.env["auditlog.rule"]
-            new_records = create_full.origin(self, vals_list, **kwargs)
-            # Take a snapshot of record values from the cache instead of using
-            # 'read()'. It avoids issues with related/computed fields which
-            # stored in the database only at the end of the transaction, but
-            # their values exist in cache.
-            new_values = {}
-            fields_list = rule_model.get_auditlog_fields(self)
-            for new_record in new_records.sudo():
-                new_values.setdefault(new_record.id, {})
-                for fname, field in new_record._fields.items():
-                    if fname not in fields_list:
-                        continue
-                    new_values[new_record.id][fname] = field.convert_to_read(
-                        new_record[fname], new_record
+                if not doing_sync:
+                    # Top create call stores a regular sync record
+                    additional_log_values.update(
+                        {
+                            "raw_args_kwargs": vals_list,  # FIXME missing store kwargs
+                            "context": self.env.context,
+                        }
                     )
-            additional_log_values = {
-                "log_type": log_type,
-                "uuid": uuid.uuid4(),
-                "timestamp": datetime.now(),
-            }
-
-            rule_model.sudo().with_context(
-                black_list_fields=black_list_fields, white_list_fields=white_list_fields
-            ).create_logs(
-                self.env.uid,
-                self._name,
-                new_records.ids,
-                "create",
-                None,
-                new_values,
-                additional_log_values,
-            )
-            context = dict(self._context)
-            if self.env.context.get("auditlog_is_capturing"):
-                context.update({"sync_ext_id": True})
-            else:
-                self.env.context = dict(self.env.context)
-                self.env.context.update({"auditlog_is_capturing": True})
-            additional_log_values = {
-                "log_type": "no_log",
-                "uuid": uuid.uuid4(),
-                "timestamp": datetime.now(),
-                "resource_ids": self.ids,
-                "raw_args_kwargs": vals_list,
-                "context": context,
-                "state": "captured",
-            }
-            rule_model.sudo().with_context(
-                black_list_fields=black_list_fields, white_list_fields=white_list_fields
-            ).create_logs(
-                self.env.uid,
-                self._name,
-                new_records.ids,
-                "create",
-                None,
-                None,
-                additional_log_values,
-            )
-            return new_records
-
-        @api.model_create_multi
-        @api.returns("self", lambda value: value.id)
-        def create_fast(self, vals_list, **kwargs):
-            self = self.with_context(auditlog_disabled=True)
-            rule_model = self.env["auditlog.rule"]
-            vals_list2 = copy.deepcopy(vals_list)
-            new_records = create_fast.origin(self, vals_list, **kwargs)
-            new_values = {}
-            for vals, new_record in zip(vals_list2, new_records):
-                new_values.setdefault(new_record.id, vals)
-            context = dict(self._context)
-            if self.env.context.get("auditlog_is_capturing"):
-                context.update({"sync_ext_id": True})
-            else:
-                self.env.context = dict(self.env.context)
-                self.env.context.update({"auditlog_is_capturing": True})
-            if log_type == "fast":
-                additional_log_values = {
-                    "log_type": log_type,
-                    "uuid": uuid.uuid4(),
-                    "timestamp": datetime.now(),
-                }
-
-                rule_model.sudo().with_context(
-                    black_list_fields=black_list_fields,
-                    white_list_fields=white_list_fields,
-                ).create_logs(
+                new_records = logged_create_call.origin(self, vals_list, **kwargs)
+                # Note that the Log is created after the call is done
+                # (and depending calls are processed)
+                self.env["auditlog.rule"].sudo().create_logs(
                     self.env.uid,
                     self._name,
                     new_records.ids,
                     "create",
-                    None,
-                    new_values,
-                    additional_log_values,
+                    additional_log_values=additional_log_values,
                 )
-            additional_log_values = {
-                "log_type": "no_log",
-                "uuid": uuid.uuid4(),
-                "timestamp": datetime.now(),
-                "resource_ids": self.ids,
-                "raw_args_kwargs": vals_list,
-                "context": context,
-                "state": "captured",
-            }
+                return new_records
 
-            rule_model.sudo().with_context(
-                black_list_fields=black_list_fields, white_list_fields=white_list_fields
-            ).create_logs(
-                self.env.uid,
-                self._name,
-                new_records.ids,
-                "create",
-                None,
-                None,
-                additional_log_values,
-            )
-            return new_records
+        return logged_create_call
 
-        return create_full if self.log_type == "full" else create_fast
-
-    def _make_write(self):
-        """Instanciate a write method that log its calls."""
+    def _make_sync_call(self, method):
+        """
+        Log a sync record for a watched method.
+        Only @api.multi methods are supported.
+        """
         self.ensure_one()
-        log_type = self.log_type
-        black_list_fields = [field.name for field in self.black_list_fields]
-        white_list_fields = [field.name for field in self.white_list_fields]
+        sync_type = self.sync_type
 
-        def write_fast(self, vals, **kwargs):
-            self = self.with_context(auditlog_disabled=True)
-            rule_model = self.env["auditlog.rule"]
-            # Log the user input only, no matter if the `vals` is updated
-            # afterwards as it could not represent the real state
-            # of the data in the database
-            vals2 = dict(vals)
-            old_vals2 = dict.fromkeys(list(vals2.keys()), False)
-            old_values = {id_: old_vals2 for id_ in self.ids}
-            new_values = {id_: vals2 for id_ in self.ids}
-            result = write_fast.origin(self, vals, **kwargs)
-            skip_sync = False
-            context = dict(self._context)
-            if self.env.context.get("auditlog_is_capturing"):
-                skip_sync = True
-            else:
-                self.env.context = dict(self.env.context)
-                self.env.context.update({"auditlog_is_capturing": True})
-            if log_type == "fast":
-                additional_log_values = {
-                    "log_type": log_type,
-                    "uuid": uuid.uuid4(),
-                    "timestamp": datetime.now(),
-                }
-
-                rule_model.sudo().with_context(
-                    black_list_fields=black_list_fields,
-                    white_list_fields=white_list_fields,
-                ).create_logs(
-                    self.env.uid,
-                    self._name,
-                    self.ids,
-                    "write",
-                    old_values,
-                    new_values,
-                    additional_log_values,
-                )
-            if not skip_sync:
+        def logged_method_call(self, *args, **kwargs):
+            doing_sync = self.env.context.get("sync_auditlog_working")
+            if sync_type and not doing_sync:
+                self = self.with_context(sync_auditlog_working=True)
                 additional_log_values = {
                     "log_type": "no_log",
                     "uuid": uuid.uuid4(),
                     "timestamp": datetime.now(),
                     "resource_ids": self.ids,
-                    "raw_args_kwargs": vals,
-                    "context": context,
+                    "raw_args_kwargs": args,  # FIXME: missing storing kwargs
+                    "context": self.env.context,
                     "state": "captured",
                 }
-
-                rule_model.sudo().with_context(
-                    black_list_fields=black_list_fields,
-                    white_list_fields=white_list_fields,
-                ).create_logs(
+                result = logged_method_call.origin(self, *args, **kwargs)
+                self.env["auditlog.rule"].sudo().create_logs(
                     self.env.uid,
                     self._name,
                     self.ids,
-                    "write",
-                    None,
-                    None,
-                    additional_log_values,
+                    method,
+                    additional_log_values=additional_log_values,
                 )
-            return result
+                return result
 
-        return write_fast
+        return logged_method_call
 
 
 class AuditlogRuleMethodCalls(models.Model):
